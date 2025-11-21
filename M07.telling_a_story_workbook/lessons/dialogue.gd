60e97d0ed7afc50a2017e65b6eefed068abf47b0
EdwinCastan <edcastanedacalde@s.sfusd.edu>
2025-11-21 11:44:15 -0800

    M7L14

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -6,6 +6,11 @@ extends Control
 @onready var body: TextureRect = %body
 @onready var expression: TextureRect = %expression
 
+var bodies := {
+	"sophia": preload ("res://assets/sophia.png"),
+	"pink": preload ("res://assets/pink.png")
+}
+
 var expressions := {
 	"happy": preload ("res://assets/emotion_happy.png"),
 	"regular": preload ("res://assets/emotion_regular.png"),
@@ -15,23 +20,43 @@ var expressions := {
 var dialogue_items: Array[Dictionary] = [
 	{
 		"expression": expressions["regular"],
-		"text": "My name is Walter Hartwell White.",
+		"text": "[wave]jesse.[/wave]",
+		"character": bodies["sophia"],
 	},
 	{
 		"expression": expressions["regular"],
-		"text": "I live at 308 Negra Arroyo Lane, Albuquerque, New Mexico, 87104.",
+		"text": "yo mista white",
+		"character": bodies["pink"],
 	},
 	{
 		"expression": expressions["sad"],
-		"text": "This is my confession.",
+		"text": "we need to [shake]cook[/shake]",
+		"character": bodies["sophia"],
+	},
+	{
+		"expression": expressions["sad"],
+		"text": "[wave]right on mr white[/wave]",
+		"character": bodies["pink"],
 	},
 	{
 		"expression": expressions["regular"],
-		"text": "If you’re reading this text, I’m probably dead–",
+		"text": "[shake]take these chemicals jesse.[/shake]",
+		"character": bodies["sophia"],
 	},
 	{
-		"expression": expressions["sad"],
-		"text": "murdered by my brother-in-law, Hank Schrader.",
+		"expression": expressions["happy"],
+		"text": "[tornado freq=1.0]you ever hear about candice?[/tornado]",
+		"character": bodies["pink"],
+	},
+	{
+		"expression": expressions["regular"],
+		"text": "[shake]Nows no time for fooling around jesse[/shake]",
+		"character": bodies["sophia"],
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "[rainbow val=0.2]ayo im just tryna joke around mr white[/rainbow]",
+		"character": bodies["pink"],
 	},
 ]
 
@@ -45,10 +70,11 @@ func show_text() -> void:
 	var current_item := dialogue_items[current_item_index]
 	rich_text_label.text = current_item["text"]
 	expression.texture = current_item["expression"]
+	body.texture = current_item["character"]
 	
 	rich_text_label.visible_ratio = 0.0
 	var tween := create_tween()
-	var text_appearing_duration := 1.2
+	var text_appearing_duration: float = current_item["text"].length() / 30.0
 	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
 	
 	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
@@ -56,6 +82,11 @@ func show_text() -> void:
 	audio_stream_player.play(sound_start_position)
 	tween.finished.connect(audio_stream_player.stop)
 	slide_in()
+	
+	next_button.disabled = true
+	tween.finished.connect(func() -> void:
+		next_button.disabled = false
+	)
 	
 func advance() -> void:
 	current_item_index += 1
